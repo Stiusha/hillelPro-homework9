445d1fe97c02cbb1ab566ad9cb1888aff03f96aa
stiusha <stiusha0110@gmail.com>
2026-05-01 19:14:14 +0300

    HM9 refactoring

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -5,7 +5,10 @@ public class DataHandler {
     public <T> String handleData(T[] items) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < items.length; i++) {
-            sb.append("(").append(i + 1).append(")\t").append(items[i].toString()).append("\t");
+            sb.append("(").append(i + 1).append(")\t").append(items[i]).append("\t");
+        }
+        if (!sb.isEmpty()) {
+            sb.deleteCharAt(sb.length() - 1);
         }
         return sb.toString();
     }
